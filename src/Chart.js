9364646c0dcc9aaa74cc824bc9451258ca47c074
debraid <de.braid@gmail.com>
2016-09-17 14:15:14 -0400

    links to metrics

--- src/Chart.js
+++ src/Chart.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 import fetchTopTenByMetric from './fetchTopTenByMetric.js';
 import topTenByMetric from './topTenByMetric.js';
 import ListOfSkaters from './ListOfSkaters.js';
@@ -6,16 +7,14 @@ import ListOfSkaters from './ListOfSkaters.js';
 class Chart extends Component {
   constructor(props){
     super(props);
-    let { statType, metric } = props.params; 
+    let skaters = { top: [], bottom: [], all: [] };
+    let { statType, metric } = props.params;
     this.state = {
-      skaters: {
-        top: [],
-        bottom: [],
-        all: [],
-      },
+      statType, 
+      metric,
+      skaters,
       uri : `http://localhost:4730/skaters/${ statType }`,
       // uri : `https://puck-api-aizjumxpky.now.sh/skaters/${ statType }`,
-      metric : metric
     }
   }
   componentDidMount() {
@@ -28,7 +27,7 @@ class Chart extends Component {
       });
   }
   componentWillReceiveProps(nextProps, nextState) {
-    console.log('nextProps', nextProps);
+    // console.log('nextProps', nextProps);
     const sk = this.state.skaters.all;
     const {metric} = nextProps.params
     this.setState({
@@ -37,24 +36,44 @@ class Chart extends Component {
     })
   }
   render() {
-    let { skaters, metric } = this.state;
+    let { skaters, metric, statType } = this.state;
     let { top, bottom } = skaters;
-    return (
-      <div>
-        <div className="App-header">
-          <h2>Charts for {metric} </h2>
-        </div>
-
-        <div className="container">
-          <div className="left">
-            <ListOfSkaters list={ top } title={"Top"} metric={metric}/>
+    let metrics = [];
+    if (top && top[0]) {
+      metrics = Object.keys(top[0]);
+    }
+    
+    if (!metric) {
+      return (<h2> Pick a metric like 
+        <Link to="skaters/corsi/CF">Corsi For (CF) </Link> or,
+        <Link to="skaters/corsi/CF60">Corsi For p60 (CF60)</Link>.
+      </h2>);
+    } else {
+      return (
+        <div>
+          <div className="App-header">
+            <h2>Charts for {metric} </h2>
+            {metrics.map((met,key)=>{
+              return (<button className="btn" type="button" key={key}>
+                <Link 
+                  to={`skaters/corsi/${ met }`}
+                  style={{textDecoration: "none", color: "white"}}>
+                  {met}
+                </Link>
+              </button>);
+            })}
           </div>
-          <div className="right">
-            <ListOfSkaters list={ bottom } title={"Bottom"} metric={metric}/>
+          <div className="container">
+            <div className="left">
+              <ListOfSkaters list={ top } title={"Top"} metric={metric}/>
+            </div>
+            <div className="right">
+              <ListOfSkaters list={ bottom } title={"Bottom"} metric={metric}/>
+            </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
 }
 
