711953c58580f4086c766bb553e490f633c7db4f
debraid <de.braid@gmail.com>
2016-09-21 11:09:09 -0400

    cleaned up topTen fn

--- src/helpers/topTen.js
+++ src/helpers/topTen.js
@@ -8,14 +8,11 @@
 import _ from 'lodash';
 
 const topTen = (arr, metric, dir) => {
-    // console.log('arr for topTen',arr);
     let res = _.sortBy(arr, metric);
-    let bottom = res.splice(0,10);
     let top = res.reverse().splice(0,10);
-    // console.log('topTen top', top);
     if (!dir) { return top }
-    // console.log('topTen bottom', bottom);
-    return bottom;
+    // return bottom 10
+    return res.splice(0,10);
 }
 
 export default topTen;
